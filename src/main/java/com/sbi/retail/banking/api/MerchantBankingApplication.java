bc9eb77063e88fb1aa9271196761d143e5b007af
Venkat <parisapoguvenkat@gmail.com>
2021-11-21 18:03:02 +0530

    Committed

---
+++ src/main/java/com/sbi/retail/banking/api/MerchantBankingApplication.java
@@ -0,0 +1,13 @@
+package com.sbi.retail.banking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbi/retail/banking/api/MerchantBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbi.retail.banking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

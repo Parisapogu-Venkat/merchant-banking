43994e6613a8fe65d4818d18716f8b53098933b7
Venkat <parisapoguvenkat@gmail.com>
2021-11-28 15:29:57 +0530

    msg

--- src/main/java/com/sbi/retail/banking/resource/CASAResource.java
+++ src/main/java/com/sbi/retail/banking/resource/CASAResource.java
@@ -17,7 +17,7 @@ public class CASAResource {
 		AccountDetails accountDetails=new AccountDetails();
 		 accountDetails=new AccountDetails();
 		accountDetails.setAccountHolderName("Pithre/Goya");
-		System.out.println("Recently added.................");
+		System.out.println("Recently text added.................");
 		accountDetails.setAccountNo("A/c123432");
 		accountDetails.setCurrentAccountBalance(20000);
 

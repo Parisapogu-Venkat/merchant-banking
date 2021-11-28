1e68e927c3ed5c8a6c30622963eda16220a3b21c
Venkat <parisapoguvenkat@gmail.com>
2021-11-28 14:16:16 +0530

    Name changed

--- src/main/java/com/sbi/retail/banking/resource/CASAResource.java
+++ src/main/java/com/sbi/retail/banking/resource/CASAResource.java
@@ -15,7 +15,7 @@ public class CASAResource {
 		System.out.println("Request from client: "+accountNumber);
 		
 		AccountDetails accountDetails=new AccountDetails();
-		accountDetails.setAccountHolderName("Francis Goya");
+		accountDetails.setAccountHolderName("Pithre Goya");
 		accountDetails.setAccountNo("A/c123432");
 		accountDetails.setCurrentAccountBalance(20000);
 		accountDetails.setSavingsAccountBalance(30000);

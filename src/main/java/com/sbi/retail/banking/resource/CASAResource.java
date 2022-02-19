15a6260e6801c1d2c5076e6d227f53ccf1d02a78
Venkat <parisapoguvenkat@gmail.com>
2022-02-19 21:00:32 +0530

    modification committed

--- src/main/java/com/sbi/retail/banking/resource/CASAResource.java
+++ src/main/java/com/sbi/retail/banking/resource/CASAResource.java
@@ -17,8 +17,7 @@ public class CASAResource {
 		AccountDetails accountDetails=new AccountDetails();
 		 accountDetails=new AccountDetails();
 		accountDetails.setAccountHolderName("Pithre/Goya");
-		System.out.println("Modifying..........");
-		System.out.println("Modified................");
+		System.out.println("Updated here.");
 		accountDetails.setAccountNo("A/c123432");
 		accountDetails.setCurrentAccountBalance(20000);
 		accountDetails.setSavingsAccountBalance(50000);

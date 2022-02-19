83111840de255b46a5a255fbde3954b0e6a4f4fd
Venkat <parisapoguvenkat@gmail.com>
2022-02-19 22:20:31 +0530

    reverted

--- src/main/java/com/sbi/retail/banking/resource/CASAResource.java
+++ src/main/java/com/sbi/retail/banking/resource/CASAResource.java
@@ -4,11 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.sbi.retail.banking.dto.AccountDetails;
 
-@RestController
 public class CASAResource {
 	@GetMapping(path = "/casa",produces = "application/json")
 	public ResponseEntity<AccountDetails> casaEnquiry(@RequestParam("accountNumber") String accountNumber) {
@@ -17,13 +15,10 @@ public class CASAResource {
 		AccountDetails accountDetails=new AccountDetails();
 		 accountDetails=new AccountDetails();
 		accountDetails.setAccountHolderName("Pithre/Goya");
-		System.out.println("Deleted here.");
-		System.out.println("Modified..........");
 		accountDetails.setAccountNo("A/c123432");
 		accountDetails.setCurrentAccountBalance(20000);
 		accountDetails.setSavingsAccountBalance(50000);
 		System.out.println("Developed");
-		System.out.println("Added");
 		return new ResponseEntity<AccountDetails>(accountDetails,HttpStatus.OK);
 	}
 }
